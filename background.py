37a5f0ad94ddbba2a60892b4269362976b4872d8
Ian Santopietro <ian@system76.com>
2019-10-22 17:05:48 -0600

    Missing comma

--- background.py
+++ background.py
@@ -41,7 +41,7 @@ class MainWindow(Gtk.Window):
         self.background_button.set_halign(Gtk.Align.CENTER)
         self.background_button.set_valign(Gtk.Align.CENTER)
         self.background_button.set_size_request(300, 150)
-        self.background_button.connect('file-set' self.on_file.set)
+        self.background_button.connect('file-set', self.on_file.set)
         
         self.lockscreen_button = Gtk.FileChooserButton()
         self.lockscreen_button.set_title("Select Lockscreen Image")
