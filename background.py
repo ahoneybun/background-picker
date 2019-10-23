36291bff3e14970d294ef0273e6099f1282c4539
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-10-22 18:06:41 -0600

    change to feh command

--- background.py
+++ background.py
@@ -100,7 +100,7 @@ class MainWindow(Gtk.Window):
         if data == "background":
             print(f'Background: {file_path}')
             # insert code for setting background here
-            subprocess.run(['ls', '-l', '-a', file_path])
+            subprocess.run(['gsettings', 'get', 'org.gnome.desktop.background', 'picture-uri', 'file:file_path'])
         elif data == "lockscreen":
             print(f'Lockscreen: {file_path}')
             # insert code for setting lockscreen here
